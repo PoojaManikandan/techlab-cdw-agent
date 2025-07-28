d6f460d49a38a906a13a26fa50994998d3d15d2d
hariprasath0622 <hariprasath0622@gmail.com>
2025-07-28 07:59:52 +0530

    changes for env location

--- client/src/components/ProtectedRoute/ProtectedRoute.js
+++ client/src/components/ProtectedRoute/ProtectedRoute.js
@@ -4,7 +4,6 @@ import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("jwtToken");
-  console.log("ProtectedRoute token:", token);
   if (!token) {
     return <Navigate to="/login" replace />;
   }
